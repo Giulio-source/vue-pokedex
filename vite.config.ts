5fc655bf3c86d6cac01c38d1d33df0d5cccfd95f
Giulio Poggia <giulio.poggia@hinto.com>
2023-10-26 17:01:52 +0200

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://giulio-source.github.io/vue-pokedex/',
   plugins: [
     vue(),
     vueJsx(),
