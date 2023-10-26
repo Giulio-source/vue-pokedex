d77d391aede831852f790046699b90f4381b29ef
Giulio Poggia <giulio.poggia@hinto.com>
2023-10-27 00:05:17 +0200

    update script

--- getPokemonImages.js
+++ getPokemonImages.js
@@ -1,32 +1,59 @@
+// @ts-nocheck
 /* eslint-env node */
-const https = require('https')
-const fs = require('fs')
-const { default: axios } = require('axios')
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
-async function getPokemonImages() {
-  const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
-  const pokemonNames = response.data.results.map((res) => res.name)
+const downloadDirectory = "./public/images";
 
-  pokemonNames.forEach((pokemon) => {
-    const imageUrl = `https://img.pokemondb.net/artwork/large/${pokemon}.jpg`
-    const imageName = `${pokemon}.jpg`
-    const file = fs.createWriteStream(`${__dirname}/public/images/${imageName}`)
-
-    https
-      .get(imageUrl, (response) => {
-        response.pipe(file)
-
-        file.on('finish', () => {
-          file.close()
-          console.log(`Image downloaded as ${imageName}`)
-        })
-      })
-      .on('error', (err) => {
-        fs.unlink(imageName, () => {
-          console.error(`Error downloading image: ${err.message}`)
-        })
-      })
-  })
+async function getPokemonNames() {
+  try {
+    const response = await axios.get(
+      "https://pokeapi.co/api/v2/pokemon?limit=151"
+    );
+    return response.data.results.map((res) => res.name);
+  } catch (error) {
+    console.error(
+      "Could not fetch pokemon from https://pokeapi.co/api/v2/pokemon?limit=151"
+    );
+  }
 }
 
-getPokemonImages()
+async function downloadImage(imageUrl, filename) {
+  try {
+    const response = await axios.get(imageUrl, { responseType: "stream" });
+    response.data.pipe(
+      fs.createWriteStream(path.join(downloadDirectory, filename))
+    );
+    console.log("\x1b[32m", filename);
+    return true;
+  } catch (error) {
+    console.error(`Failed to download ${filename}: ${error.message}`);
+    return false;
+  }
+}
+
+async function downloadImages(imageUrls) {
+  const successfulDownloads = [];
+  for (const imageUrl of imageUrls) {
+    const filename = path.basename(imageUrl);
+    const downloaded = await downloadImage(imageUrl, filename);
+    if (downloaded) {
+      successfulDownloads.push(filename);
+    }
+  }
+  return successfulDownloads;
+}
+
+if (!fs.existsSync(downloadDirectory)) {
+  fs.mkdirSync(downloadDirectory);
+}
+
+(async () => {
+  const pokemon = await getPokemonNames();
+  const imageUrls = pokemon.map(
+    (pokemon) => `https://img.pokemondb.net/artwork/large/${pokemon}.jpg`
+  );
+  const successfulDownloads = await downloadImages(imageUrls);
+  console.log("\x1b[42m\x1b[30m",  `Successfully downloaded ${successfulDownloads.length} images `);
+})();
